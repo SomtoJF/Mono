ddaf5bfc72a36d7b9ab7a25c93a398b820074d03
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-02-14 10:54:06 +0100

    Enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -11,6 +11,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
   const port = process.env.PORT || 8080;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/`);
