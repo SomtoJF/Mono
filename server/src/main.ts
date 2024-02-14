51a21392a09ed82e134d89b6f92f4b4cf8115837
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-02-14 10:38:58 +0100

    Enable class valiadtion

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,13 +3,14 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const port = process.env.PORT || 8080;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/`);
